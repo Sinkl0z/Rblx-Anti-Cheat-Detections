374d1b82035cb1d73f9b81032d48e15250a51414
Sinkl0z <synapseforx@gmail.com>
2025-06-01 00:20:51 +0200

    Update Protected Bypass.lua

--- EnvironmentTakeover/newproxy/Protected Bypass.lua
+++ EnvironmentTakeover/newproxy/Protected Bypass.lua
@@ -1,1 +1,12 @@
 -- Bypass code of the protected newproxy detection version
+
+local OldPcall
+OldPcall = hookfunction(getrenv().pcall, function(Function, Level, ...)
+    if Function == getfenv then
+        if Level >= 5 then
+            return true, select(2, OldPcall(Function, Level, ...))
+        end
+        return false, select(2, OldPcall(Function, Level, ...))
+    end
+    return OldPcall(Function, ...)
+end)
