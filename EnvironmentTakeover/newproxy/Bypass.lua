564de84c719e48810f88677fdbad3a794a5daf4c
Sinkl0z <synapseforx@gmail.com>
2025-05-31 23:53:38 +0200

    Update Bypass.lua

--- EnvironmentTakeover/newproxy/Bypass.lua
+++ EnvironmentTakeover/newproxy/Bypass.lua
@@ -1,1 +1,9 @@
 
+
+local OldPcall
+OldPcall = hookfunction(getrenv().pcall, function(Function, ...)
+    if Function == getfenv then
+        return false, select(2, OldPcall(Function, ...))
+    end
+    return OldPcall(Function, ...)
+end)
